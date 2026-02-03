d0a822882099a8edd986c445a9bf71f1162228ea
paulc <212168+paulc@users.noreply.github.com>
2026-02-03 23:54:29 +0000

    macro

--- examples/macro.rs
+++ examples/macro.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
-use rquickjs::{CatchResultExt, Context, Runtime};
+use rquickjs::class::Trace;
+use rquickjs::{ArrayBuffer, CatchResultExt, Class, Context, Ctx, JsLifetime, Runtime, Value};
 
 #[rquickjs::module(rename_vars = "camelCase")]
 mod native_api {
@@ -28,6 +29,61 @@ mod native_api {
     }
 }
 
+#[derive(Trace, JsLifetime, Clone, Debug)]
+#[rquickjs::class]
+pub struct TestClass {
+    #[qjs(get, set)]
+    pub name: String,
+    pub data: Vec<u8>,
+}
+
+#[rquickjs::methods]
+impl TestClass {
+    #[qjs(get, rename = "data")]
+    pub fn get_data<'js>(&self, ctx: Ctx<'js>) -> rquickjs::Result<ArrayBuffer<'js>> {
+        ArrayBuffer::new(ctx.clone(), self.data.clone())
+    }
+    #[qjs(get, rename = "text")]
+    pub fn get_text(&self) -> rquickjs::Result<String> {
+        Ok(String::from_utf8(self.data.clone())?)
+    }
+    #[qjs(set, rename = "text")]
+    pub fn set_text(&mut self, text: String) -> rquickjs::Result<()> {
+        self.data = text.as_bytes().to_vec();
+        Ok(())
+    }
+    pub fn format(&self) -> rquickjs::Result<String> {
+        Ok(format!("{:?}", self))
+    }
+}
+
+#[derive(Trace, JsLifetime, Clone, Debug)]
+#[rquickjs::class]
+pub enum TestEnum {
+    A(u8),
+    B(u8),
+}
+
+#[rquickjs::methods]
+impl TestEnum {
+    pub fn get_type(&self) -> rquickjs::Result<String> {
+        match self {
+            TestEnum::A(_) => Ok("A".into()),
+            TestEnum::B(_) => Ok("B".into()),
+        }
+    }
+}
+
+#[rquickjs::function]
+fn print_v<'js>(ctx: Ctx<'js>, v: Value<'js>) -> rquickjs::Result<()> {
+    let output = ctx
+        .json_stringify(v)?
+        .and_then(|s| s.as_string().map(|s| s.to_string().ok()).flatten())
+        .unwrap_or_else(|| "<ERR>".to_string());
+    println!("{}", output);
+    Ok(())
+}
+
 #[rquickjs::function]
 pub fn print(s: String) {
     println!("{s}");
@@ -50,6 +106,15 @@ fn main() -> Result<()> {
               print(`Exception: ${e.message}`);
             }
 
+            const o = { a:1, b:[1,2,3], c:false };
+            print_v(e.get_type());
+            print_v(t.data.byteLength);
+            print_v(t.text);
+            const v = new Int8Array(t.data);
+            print_v(v);
+            t.text = "CHANGED";
+            print(t.format());
+
             throw_ex("BYE");
 
             print("END");
@@ -58,6 +123,20 @@ fn main() -> Result<()> {
     let r = ctx.with::<_, Result<()>>(|ctx| {
         rquickjs::Module::declare_def::<js_native_api, _>(ctx.clone(), "native_api").unwrap();
         ctx.globals().set("print", js_print).unwrap();
+        ctx.globals().set("print_v", js_print_v).unwrap();
+
+        let cls = Class::instance(
+            ctx.clone(),
+            TestClass {
+                name: "Test".into(),
+                data: "HELLO".as_bytes().to_vec(),
+            },
+        );
+
+        let e = TestEnum::A(99);
+
+        ctx.globals().set("t", cls)?;
+        ctx.globals().set("e", e)?;
 
         let m = rquickjs::Module::declare(ctx.clone(), "script", script)
             .catch(&ctx)
